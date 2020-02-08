d500b171689d9ac84f9cb3b4fe0f0f71aeb5b208
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2020-02-08 14:56:10 +0900

    lodash-esでやってみる

--- tree_shaking/src/index.js
+++ tree_shaking/src/index.js
@@ -1,6 +1,7 @@
 import {
   cube
 } from './math.js';
+import _ from 'lodash-es'
 
 function component() {
   const element = document.createElement('pre');
@@ -8,6 +9,7 @@ function component() {
   element.innerHTML = [
      'Hello webpack!',
      '5 cubed is equal to ' + cube(5),
+    //  '_.isEmpty(null) is equal to ' + _.isEmpty(null),  // この一文の有無でtree-shakingの有無が変わるよ
    ].join('\n\n');
 
   return element;
